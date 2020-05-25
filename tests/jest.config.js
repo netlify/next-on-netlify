f3af3ff16c213ea8ac1aecf495c9e2b6cfc18c19
Finn Woelm <public@finnwoelm.com>
2020-05-25 20:50:16 +0200

    Jest: Fix jest-haste-map warning
    Tell Jest to ignore package.json files in tests/build/ and in the
    cypress/ folder. Otherwise, it prints out a warning about duplicate
    names used in package.json.
    
    Example:
    jest-haste-map: Haste module naming collision: next-on-netlify-test
      The following files share their name; please adjust your hasteImpl:
        * <rootDir>/fixtures/package.json
        * <rootDir>/builds/customNextDistDir.test/package.json

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  // Do not scan the ./builds/ folder for tests or package.json files
-  testPathIgnorePatterns: ["./builds/"]
+  // Do not scan the ./builds/ folder and the Cypress folder for tests or
+  // package.json files
+  modulePathIgnorePatterns: ["./builds/", "<rootDir>/cypress/"]
 };

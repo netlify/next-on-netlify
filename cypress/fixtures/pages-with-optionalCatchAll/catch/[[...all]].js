85ec0f2cb7d238c93a9c88b21f73114c908c32e2
Raj Nigam <rajington@users.noreply.github.com>
2020-06-10 01:35:11 -0400

    support optional catch-all routes
    Next.js added a new, experimental, [optional catch-all routes](https://nextjs.org/docs/api-routes/dynamic-api-routes#optional-catch-all-api-routes), this adds support for it by looking for one more wrapping square bracket

---
+++ cypress/fixtures/pages-with-optionalCatchAll/catch/[[...all]].js
@@ -0,0 +1,50 @@
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+
+const CatchAll = ({ show }) => {
+  const router = useRouter()
+
+  // This is never shown on Netlify. We just need it for NextJS to be happy,
+  // because NextJS will render a fallback HTML page.
+  if (router.isFallback) {
+    return <div>Loading...</div>
+  }
+
+  return (
+    <div>
+      <p>
+        This page uses getStaticProps() to pre-fetch a TV show.
+      </p>
+
+      <hr/>
+
+      <h1>Show #{show.id}</h1>
+      <p>
+        {show.name}
+      </p>
+
+      <hr/>
+
+      <Link href="/">
+        <a>Go back home</a>
+      </Link>
+    </div>
+  )
+}
+
+export async function getServerSideProps({ params }) {
+  // The ID to render
+  const { all } = params
+  const id = all ? all[0] : 1
+
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+  const data = await res.json();
+
+  return {
+    props: {
+      show: data
+    }
+  }
+}
+
+export default CatchAll
---
+++ tests/optionalCatchAll.test.js
@@ -0,0 +1,79 @@
+// Test that next-on-netlify does not crash when pre-rendering index.js file.
+// See: https://github.com/FinnWoelm/next-on-netlify/issues/2#issuecomment-636415494
+
+const { parse, join } = require('path')
+const { copySync, emptyDirSync, existsSync,
+        readdirSync, readFileSync, readJsonSync } = require('fs-extra')
+const npmRunBuild = require("./helpers/npmRunBuild")
+
+// The name of this test file (without extension)
+const FILENAME = parse(__filename).name
+
+// The directory which will be used for testing.
+// We simulate a NextJS app within that directory, with pages, and a
+// package.json file.
+const PROJECT_PATH = join(__dirname, "builds", FILENAME)
+
+// The directory that contains the fixtures, such as NextJS pages,
+// NextJS config, and package.json
+const FIXTURE_PATH = join(__dirname, "fixtures")
+
+// Capture the output of `npm run build` to verify successful build
+let BUILD_OUTPUT
+
+beforeAll(
+  async () => {
+    // Clear project directory
+    emptyDirSync(PROJECT_PATH)
+    emptyDirSync(join(PROJECT_PATH, "pages"))
+
+    // Copy NextJS pages and config
+    copySync(
+      join(FIXTURE_PATH, "pages-with-optionalCatchAll"),
+      join(PROJECT_PATH, "pages")
+    )
+    copySync(
+      join(FIXTURE_PATH, "next.config.js-with-optionalCatchAll"),
+      join(PROJECT_PATH, "next.config.js")
+    )
+
+    // Copy package.json
+    copySync(
+      join(FIXTURE_PATH, "package.json"),
+      join(PROJECT_PATH, "package.json")
+    )
+
+    // Invoke `npm run build`: Build Next and run next-on-netlify
+    const { stdout } = await npmRunBuild({ directory: PROJECT_PATH })
+    BUILD_OUTPUT = stdout
+  },
+  // time out after 180 seconds
+  180 * 1000
+)
+
+describe('Next', () => {
+  test('builds successfully', () => {
+    // NextJS output
+    expect(BUILD_OUTPUT).toMatch("Creating an optimized production build...")
+    expect(BUILD_OUTPUT).toMatch("Automatically optimizing pages...")
+    expect(BUILD_OUTPUT).toMatch("First Load JS shared by all")
+
+    // Next on Netlify output
+    expect(BUILD_OUTPUT).toMatch("Next on Netlify")
+    expect(BUILD_OUTPUT).toMatch("Success! All done!")
+  })
+})
+
+describe('Routing',() => {
+  test('creates Netlify redirects', async () => {
+    // Read _redirects file
+    const contents = readFileSync(join(PROJECT_PATH, "out_publish", "_redirects"))
+    let redirects = contents.toString()
+
+    // Replace non-persistent build ID with placeholder
+    redirects = redirects.replace(/\/_next\/data\/[^\/]+\//g, "/_next/data/%BUILD_ID%/")
+
+    // Check that redirects match
+    expect(redirects).toMatchSnapshot()
+  })
+})

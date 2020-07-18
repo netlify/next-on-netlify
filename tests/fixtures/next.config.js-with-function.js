5c896eed5bd14a5435ea7c977a7fafea644fc152
Finn Woelm <public@finnwoelm.com>
2020-07-18 20:56:41 +0200

    Add support for function in next.config.js
    The configuration for NextJS in next.config.js can also be supplied as
    a function (in addition to an object). This commit adds support for
    reading the distDir option when the NextJS config is set by a function.
    
    See: https://github.com/FinnWoelm/next-on-netlify/issues/25

---
+++ tests/fixtures/next.config.js-with-function.js
@@ -0,0 +1,15 @@
+const { PHASE_PRODUCTION_BUILD } = require('next/constants')
+
+module.exports = (phase, { defaultConfig }) => {
+  // next-on-netlify uses settings from PHASE_PRODUCTION_BUILD
+  // This is the same phase that is used when running `next build`
+  if (phase === PHASE_PRODUCTION_BUILD) {
+    return {
+      target: 'serverless',
+      distDir: '.myCustomDir'
+    }
+  }
+
+  // Default options
+  return {}
+}

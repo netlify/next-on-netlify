7daaaed9e09475f369e0d90182e75ea7720c5b12
erezrokah <erezrokah@users.noreply.github.com>
2021-02-01 18:44:19 +0100

    feat: add watch mode

--- next-on-netlify.js
+++ next-on-netlify.js
@@ -1,17 +1,28 @@
 #!/usr/bin/env node
 const { program } = require("commander");
 
-program
-  .option(
-    "--max-log-lines [number]",
-    "lines of build output to show for each section",
-    50
-  )
-  .parse(process.argv);
-
 const nextOnNetlify = require("./index");
 const { logTitle } = require("./lib/helpers/logger");
 
-nextOnNetlify();
+program.option(
+  "--max-log-lines [number]",
+  "lines of build output to show for each section",
+  50
+);
 
-logTitle("✅ Success! All done!");
+program
+  .command("watch")
+  .description("re-runs next-on-netlify on changes")
+  .action(() => {
+    nextOnNetlify({ watch: true });
+  });
+
+program
+  .command("build", { isDefault: true })
+  .description("runs next-on-netlify")
+  .action(() => {
+    nextOnNetlify();
+    logTitle("✅ Success! All done!");
+  });
+
+program.parse(process.argv);
